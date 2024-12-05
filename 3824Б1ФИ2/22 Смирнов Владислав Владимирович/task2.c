19a59a877af19e54bfa06a453b4775c77d8ab41c
79502 <vladsmirnov8583@gmail.com>
2024-12-05 09:52:35 +0300

    Update task2.c
    Смирнов Владислав Владимирович

--- 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task2.c
+++ 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task2.c
@@ -1,6 +1,18 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include<limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+    int pr;
+    pr = a * b;
+    if ((a >= INT_MAX) || (b >= INT_MAX))
+        pr = INT_MAX; else {
+        if (pr >= INT_MAX)
+            pr = INT_MAX;
+    }
+    return pr;
+}
+int main() {
+    int a = 10, b = 3, pro;
+    pro = product_of_two_numbers (a, b);
 }
