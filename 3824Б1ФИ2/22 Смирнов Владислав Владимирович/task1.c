7ad8bf8db60b0ce40d92b492906d6d41a9921556
79502 <vladsmirnov8583@gmail.com>
2024-12-05 09:40:19 +0300

    Update task1.c
    Смирнов Владислав Владимирович

--- 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task1.c
+++ 3824Б1ФИ2/22 Смирнов Владислав Владимирович/task1.c
@@ -1,7 +1,24 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include<limits.h>
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  int sum;
+  sum = a+b;
+  if ((a>=INT_MAX) || (b>=INT_MAX))
+    sum = INT_MAX; else {
+  if (sum >= INT_MAX)
+    sum = INT_MAX;
+    }
+    if ((a<=INT_MIN) || (b<=INT_MIN))
+    sum = INT_MIN; else {
+  if (sum <= INT_MIN)
+    sum = INT_MIN;
+    }
+  return sum;
+}
+int main() {
+  int a = 10, b = 3, summa;
+  summa = sum_of_two_numbers(a, b);
 }
